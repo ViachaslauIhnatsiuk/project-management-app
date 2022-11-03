3e1485eaf8dc3a8de728fa1c24aeec6d284c34d2
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-11-03 18:35:03 +0300

    feat: add log-in base template and auth.module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { LogInComponent } from './pages/log-in/log-in.component';
+
+
+
+@NgModule({
+  declarations: [
+    SignUpComponent,
+    LogInComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthModule { }
