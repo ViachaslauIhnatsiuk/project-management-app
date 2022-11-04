e20ba1dfd982e411cf760a9d644583b198242d8a
Viachaslau <viachaslauihnatsiuk@gmail.com>
2022-11-04 14:51:05 +0300

    feat: implement adaptive layout for welcome page

--- src/app/core/pages/welcome-page/welcome-page.component.ts
+++ src/app/core/pages/welcome-page/welcome-page.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import {
+  kanbanPracticeCards,
+  productivityList,
+  cusomizeParagraphs,
+  rssParagraphs,
+  developers,
+} from 'src/app/core/constants/welcome-page-content.constants';
+import { KanbanPracticeCard, Developer } from 'src/app/core/models/welcome-page-content.models';
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
   styleUrls: ['./welcome-page.component.scss'],
 })
-export class WelcomePageComponent {}
+export class WelcomePageComponent {
+  public readonly kanbanPracticeCards: KanbanPracticeCard[] = kanbanPracticeCards;
+  public readonly productivityList: string[] = productivityList;
+  public readonly cusomizeParagraphs: string[] = cusomizeParagraphs;
+  public readonly rssParagraphs: string[] = rssParagraphs;
+  public readonly developers: Developer[] = developers;
+}
