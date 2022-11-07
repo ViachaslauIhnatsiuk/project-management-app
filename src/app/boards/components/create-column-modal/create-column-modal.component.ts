af72e2a369586881588005e0b24abad94c25dd88
GlebKolyano <glebkolyano38@gmail.com>
2022-11-07 10:42:34 +0300

    feat: implement changing the column name

--- src/app/boards/components/create-column-modal/create-column-modal.component.ts
+++ src/app/boards/components/create-column-modal/create-column-modal.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
-import {
-  INITIAL_INPUT_VALUE,
-  MAX_TITLE_LENGTH,
-  MIN_INPUT_VALUE_LENGTH,
-} from '../../constants/create-board-modal.constants';
 import { CreateColumnComponent } from '../create-column/create-column.component';
+import { MAX_TITLE_LENGTH, MIN_TITLE_LENGTH } from '../../constants/column-item.constants';
+import { INITIAL_INPUT_VALUE } from '../../constants/create-board-modal.constants';
 
 @Component({
   selector: 'app-create-column-modal',
@@ -24,7 +21,7 @@ export class CreateColumnModalComponent {
     this.form = new FormGroup({
       title: new FormControl(INITIAL_INPUT_VALUE, [
         Validators.required,
-        Validators.minLength(MIN_INPUT_VALUE_LENGTH),
+        Validators.minLength(MIN_TITLE_LENGTH),
         Validators.maxLength(MAX_TITLE_LENGTH),
       ]),
     });
