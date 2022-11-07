bccc1fef6a0d902351bf2ade3d8b24209c0b64f1
Viachaslau <viachaslauihnatsiuk@gmail.com>
2022-11-07 13:01:44 +0300

    feat: add footer component

---
+++ src/app/core/constants/footer.constants.ts
@@ -0,0 +1,14 @@
+export const githubAccounts = [
+  {
+    name: 'Dzmitry Dauhaliavets',
+    link: 'https://github.com/Dauhaliavets',
+  },
+  {
+    name: 'Viachaslau Ihnatsiuk',
+    link: 'https://github.com/ViachaslauIhnatsiuk',
+  },
+  {
+    name: 'Gleb Kolyano',
+    link: 'https://github.com/GlebKolyano',
+  },
+];
