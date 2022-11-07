3a346ee555095c07104b1ec9857a19b2ca6dbd11
GlebKolyano <glebkolyano38@gmail.com>
2022-11-07 10:42:34 +0300

    feat: implement changing the column name

--- src/app/boards/boards.module.ts
+++ src/app/boards/boards.module.ts
@@ -1,25 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { StoreModule } from '@ngrx/store';
 
 import { BoardsRoutingModule } from './boards-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { BoardsPageComponent } from './pages/boards-page/boards-page.component';
-import { BoardCardComponent } from './components/board-card/board-card.component';
 import { CreateBoardComponent } from './components/create-board/create-board.component';
 import { BoardsComponent } from './components/boards/boards.component';
+import { BoardCardComponent } from './components/board-card/board-card.component';
 import { CreateBoardModalComponent } from './components/create-board-modal/create-board-modal.component';
 import { UpdateBoardModalComponent } from './components/update-board-modal/update-board-modal.component';
 import { BoardService } from './services/board.service';
-import { boardsReducer } from './store/reducers/boards.reducers';
-import { BoardsEffects } from './store/effects/boards.effects';
 import { ProjectPageComponent } from './pages/project-page/project-page.component';
 import { ColumnService } from './services/column.service';
 import { BoardsEffects } from './store/effects/boards.effects';
 import { ColumnEffects } from './store/effects/column.effects';
+import { boardsReducer } from './store/reducers/boards.reducers';
+import { ColumnsComponent } from './components/columns/columns.component';
+import { ColumnItemComponent } from './components/column-item/column-item.component';
+import { CreateColumnComponent } from './components/create-column/create-column.component';
+import { CreateColumnModalComponent } from './components/create-column-modal/create-column-modal.component';
 
 @NgModule({
   declarations: [
@@ -30,9 +33,14 @@ import { ColumnEffects } from './store/effects/column.effects';
     UpdateBoardModalComponent,
     CreateBoardComponent,
     ProjectPageComponent,
+    ColumnsComponent,
+    ColumnItemComponent,
+    CreateColumnComponent,
+    CreateColumnModalComponent,
   ],
   exports: [BoardsPageComponent],
   imports: [
+    StoreModule.forFeature('boards', boardsReducer),
     EffectsModule.forFeature([BoardsEffects, ColumnEffects]),
     CommonModule,
     BoardsRoutingModule,
--- src/app/boards/components/column-item/column-item.component.ts
+++ src/app/boards/components/column-item/column-item.component.ts
@@ -1,18 +1,23 @@
-import { Component, Input, OnDestroy } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
-import { INITIAL_ID_BOARD_VALUE } from '../../constants/column-item.constants';
+import { updateColumn } from '../../store/actions/column.actions';
 import { IColumn } from '../../models/boards.models';
+import {
+  INITIAL_ID_BOARD_VALUE,
+  MAX_TITLE_LENGTH,
+  MIN_TITLE_LENGTH,
+} from '../../constants/column-item.constants';
 
 @Component({
   selector: 'app-column-item',
   templateUrl: './column-item.component.html',
   styleUrls: ['./column-item.component.scss'],
 })
-export class ColumnItemComponent implements OnDestroy {
+export class ColumnItemComponent implements OnDestroy, OnInit {
   @Input() public column!: IColumn;
 
   public form!: FormGroup;
@@ -29,8 +34,36 @@ export class ColumnItemComponent implements OnDestroy {
     );
   }
 
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
   public toggleEditMode(): void {
+    this.initializeForm();
     this.isEditMode = !this.isEditMode;
+  }
+
+  private initializeForm(): void {
+    this.form = new FormGroup({
+      title: new FormControl(this.column.title, [
+        Validators.required,
+        Validators.minLength(MIN_TITLE_LENGTH),
+        Validators.maxLength(MAX_TITLE_LENGTH),
+      ]),
+    });
+  }
+
+  public updateTitle(): void {
+    const isNewTitleEquelPrevValue = this.column.title === this.form.value.title;
+
+    if (this.form.valid && !isNewTitleEquelPrevValue) {
+      const column: IColumn = { ...this.column, ...this.form.value };
+      this.store.dispatch(updateColumn({ props: { column, idBoard: this.idBoard } }));
+    }
+
+    if (this.form.valid) {
+      this.toggleEditMode();
+    }
   }
 
   ngOnDestroy(): void {
