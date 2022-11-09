7a02a3ea106993ff9919873c0a7b436a8c0d4789
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-11-09 10:08:19 +0300

    refactor: after review

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -15,19 +15,17 @@ export class AuthService {
   constructor(private http: HttpClient, private router: Router) {}
 
   signUp(fields: ISignUpRequest) {
-    this.http.post<ISignUpResponse>('signup', { ...fields }).subscribe((data) => {
+    this.http.post<ISignUpResponse>('signup', fields).subscribe((data) => {
       localStorage.setItem('userName', fields.name);
       this.router.navigate(['auth/logIn']);
-      console.log('SIGN UP RESPONSE: ', data);
     });
   }
 
   signIn(fields: ILogInRequest) {
-    this.http.post<ILogInResponse>('signin', { ...fields }).subscribe((data) => {
+    this.http.post<ILogInResponse>('signin', fields).subscribe((data) => {
       localStorage.setItem('userLogin', fields.login);
       localStorage.setItem('token', data.token);
       this.router.navigate(['']);
-      console.log('LOG IN RESPONSE: ', data);
     });
   }
 
