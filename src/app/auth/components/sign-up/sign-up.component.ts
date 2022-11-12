36eb564928ab60396a01c11732f5b1100f563626
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-11-13 02:46:00 +0300

    fix: minor fix signUp component

--- src/app/auth/components/sign-up/sign-up.component.ts
+++ src/app/auth/components/sign-up/sign-up.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { ISignUpRequest } from 'src/app/core/models/auth-interceptor.models';
 import { signUp } from '../../store/actions/auth.actions';
+import { IAuthStateError } from '../../store/models/auth.models';
+import { selectError, selectIsLoading } from '../../store/selectors/auth.selectors';
 
 @Component({
   selector: 'app-sign-up',
@@ -16,8 +19,15 @@ export class SignUpComponent {
     login: ['', Validators.required],
     password: ['', Validators.required],
   });
+  loading$!: Observable<boolean>;
+  error$!: Observable<IAuthStateError>;
 
   constructor(private fb: FormBuilder, private router: Router, private store: Store) {}
+
+  ngOnInit() {
+    this.loading$ = this.store.select(selectIsLoading);
+    this.error$ = this.store.select(selectError);
+  }
 
   onSubmit() {
     const fieldValues = this.signUpForm.value as ISignUpRequest;
