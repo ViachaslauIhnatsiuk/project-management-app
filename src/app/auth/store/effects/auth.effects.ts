ae7aa4cee7b9312bc0d9a1ce44a99f9de883d6a1
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-11-12 18:11:28 +0300

    refactor: auth-interceptor

--- src/app/auth/store/effects/auth.effects.ts
+++ src/app/auth/store/effects/auth.effects.ts
@@ -1,8 +1,9 @@
-import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
 import { of } from 'rxjs';
-import { map, catchError, switchMap } from 'rxjs/operators';
+import { map, catchError, switchMap, tap } from 'rxjs/operators';
 import { AuthService } from '../../services/auth.service';
 import {
   logIn,
@@ -12,10 +13,16 @@ import {
   signUpError,
   signUpSuccess,
 } from '../actions/auth.actions';
+import { IAuthState } from '../models/auth.models';
 
 @Injectable()
 export class AuthEffects {
-  constructor(private actions$: Actions, private authService: AuthService) {}
+  constructor(
+    private actions$: Actions,
+    private authService: AuthService,
+    private router: Router,
+    private store: Store<IAuthState>,
+  ) {}
 
   logIn$ = createEffect(() => {
     return this.actions$.pipe(
@@ -23,6 +30,7 @@ export class AuthEffects {
       switchMap(({ login, password }) => {
         return this.authService.signIn({ login, password }).pipe(
           map(({ token }) => logInSuccess({ token })),
+          tap(() => this.router.navigate([''])),
           catchError(({ error: { statusCode, message } }) => {
             return of(logInError({ statusCode, message }));
           }),
@@ -36,7 +44,10 @@ export class AuthEffects {
       ofType(signUp),
       switchMap(({ name, login, password }) => {
         return this.authService.signUp({ name, login, password }).pipe(
-          map(({ _id, name, login }) => signUpSuccess({ _id, name, login })),
+          map(({ _id, name, login }) => {
+            this.store.dispatch(logIn({ login, password }));
+            return signUpSuccess({ _id, name, login });
+          }),
           catchError(({ error: { statusCode, message } }) =>
             of(signUpError({ statusCode, message })),
           ),
--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -5,9 +5,10 @@ import {
   HttpEvent,
   HttpInterceptor,
   HttpErrorResponse,
+  HttpResponse,
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { BASE_URL } from 'src/app/core/constants/interceptors.constants';
 import {
   ILogInRequest,
@@ -16,30 +17,29 @@ import {
   ISignUpRequest,
   ISignUpResponse,
 } from 'src/app/core/models/auth-interceptor.models';
-import { ErrorHandlerService } from 'src/app/core/services/error-handler.service';
+import { ResponseHandlerService } from 'src/app/core/services/response-handler.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private errorHandlerService: ErrorHandlerService) {}
+  constructor(private responseHandlerService: ResponseHandlerService) {}
   intercept(
     request: HttpRequest<ILogInRequest | ISignUpRequest>,
     next: HttpHandler,
   ): Observable<HttpEvent<ILogInResponse | ISignUpResponse | IResponseError>> {
-    const url = `${BASE_URL}auth/${request.url}`;
-
-    const modifyRequest = request.clone({
-      url,
-      setHeaders: {
-        accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    });
-
     if (request.url.includes('signup')) {
-      return next.handle(modifyRequest).pipe(
+      return next.handle(this.getModifiedRequest(request)).pipe(
+        tap((data) => {
+          if (data instanceof HttpResponse) {
+            this.responseHandlerService.handleResponse(data.status, 'New user is created');
+          }
+        }),
         catchError((error) => {
           if (error instanceof HttpErrorResponse) {
-            this.errorHandlerService.handleError(error);
+            const {
+              status,
+              error: { message },
+            } = error;
+            this.responseHandlerService.handleResponse(status, message);
           }
           return throwError(() => error);
         }),
@@ -47,10 +47,19 @@ export class AuthInterceptor implements HttpInterceptor {
     }
 
     if (request.url.includes('signin')) {
-      return next.handle(modifyRequest).pipe(
+      return next.handle(this.getModifiedRequest(request)).pipe(
+        tap((data) => {
+          if (data instanceof HttpResponse) {
+            this.responseHandlerService.handleResponse(data.status, 'Successeful login');
+          }
+        }),
         catchError((error) => {
           if (error instanceof HttpErrorResponse) {
-            this.errorHandlerService.handleError(error);
+            const {
+              status,
+              error: { message },
+            } = error;
+            this.responseHandlerService.handleResponse(status, message);
           }
           return throwError(() => error);
         }),
@@ -58,5 +67,19 @@ export class AuthInterceptor implements HttpInterceptor {
     }
 
     return next.handle(request);
+  }
+
+  private getModifiedRequest(
+    request: HttpRequest<ILogInRequest | ISignUpRequest>,
+  ): HttpRequest<ILogInRequest | ISignUpRequest> {
+    const url = `${BASE_URL}auth/${request.url}`;
+
+    return request.clone({
+      url,
+      setHeaders: {
+        accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
   }
 }
