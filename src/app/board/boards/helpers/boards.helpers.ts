37f0b0257dcb6dfa095f591200b6c46c75d6c22a
Viachaslau <viachaslauihnatsiuk@gmail.com>
2022-11-14 14:30:24 +0300

    refactor: refactor routing, boards helper

--- src/app/board/boards/helpers/boards.helpers.ts
+++ src/app/board/boards/helpers/boards.helpers.ts
@@ -1,19 +1,11 @@
+import jwtDecode from 'jwt-decode';
+
 const getUserId = (): string | null => {
-  const userData = localStorage.getItem('userData');
-  if (userData) {
-    const userId: string | null = JSON.parse(userData).id;
-    return userId;
-  }
-  return null;
+  const TOKEN = window.localStorage.getItem('token') as string;
+  const { id } = jwtDecode<{ id: string }>(TOKEN);
+  return id;
 };
 
-const getToken = (): string | null => {
-  const userData = localStorage.getItem('userData');
-  if (userData) {
-    const token: string | null = JSON.parse(userData).token;
-    return token;
-  }
-  return null;
-};
+const getToken = (): string | null => window.localStorage.getItem('token');
 
 export { getUserId, getToken };
