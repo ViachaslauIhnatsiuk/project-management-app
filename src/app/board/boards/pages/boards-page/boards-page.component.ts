c9aedb868a57720f19ee1abe2f6f932c0c100e34
Viachaslau <viachaslauihnatsiuk@gmail.com>
2022-11-14 10:41:03 +0300

    fix: fix bugs

--- src/app/board/boards/pages/boards-page/boards-page.component.ts
+++ src/app/board/boards/pages/boards-page/boards-page.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { getUser } from 'src/app/auth/store/actions/auth.actions';
-import { selectToken } from 'src/app/auth/store/selectors/auth.selectors';
 
 @Component({
   selector: 'app-boards-page',
@@ -13,9 +12,8 @@ export class BoardsPageComponent implements OnInit {
   constructor(private authService: AuthService, private store: Store) {}
 
   ngOnInit(): void {
-    this.store.select(selectToken).subscribe((token) => {
-      const { id } = this.authService.getUserDataFromToken(token);
-      this.store.dispatch(getUser({ userId: id }));
-    });
+    const TOKEN = window.localStorage.getItem('token') as string;
+    const { id } = this.authService.getUserDataFromToken(TOKEN);
+    this.store.dispatch(getUser({ userId: id }));
   }
 }
--- src/app/core/interceptors/boards.interceptor.ts
+++ src/app/core/interceptors/boards.interceptor.ts
@@ -8,21 +8,31 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { getToken } from 'src/app/board/boards/helpers/boards.helpers';
+import { BASE_URL } from 'src/app/core/constants/interceptors.constants';
 
 @Injectable()
 export class BoardsInterceptor implements HttpInterceptor {
   public token = getToken();
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    if (request.url.includes('boards')) {
+    if (
+      request.url.includes('boards') ||
+      request.url.includes('columns') ||
+      request.url.includes('tasks') ||
+      request.url.includes('columnsSet') ||
+      request.url.includes('tasksSet')
+    ) {
+      const url = `${BASE_URL}${request.url}`;
+      const TOKEN = window.localStorage.getItem('token');
       let headers: HttpHeaders = request.headers;
 
-      if (this.token) {
-        headers = request.headers.set('Authorization', `Bearer ${this.token}`);
+      if (TOKEN) {
+        headers = request.headers.set('Authorization', `Bearer ${TOKEN}`);
       }
 
       return next.handle(
         request.clone({
+          url,
           headers,
         }),
       );
