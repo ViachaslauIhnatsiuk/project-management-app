c79276ddd1b3abc8dc93ac0eb99d2eabd91360da
Viachaslau <viachaslauihnatsiuk@gmail.com>
2022-11-14 15:27:56 +0300

    feat: add display of existing boards in the sidebar

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,7 +1,12 @@
+import { IBoard } from 'src/app/board/boards/models/boards.models';
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { HeaderService } from 'src/app/core/services/header.service';
 import { listItems } from 'src/app/core/constants/sidebar.constants';
 import { IListItems } from 'src/app/core/models/sidebar.models';
+import { selectBoards } from 'src/app/board/boards/store/selectors/boards.selectors';
+import { getBoards } from 'src/app/board/boards/store/actions/boards.actions';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,6 +15,21 @@ import { IListItems } from 'src/app/core/models/sidebar.models';
 })
 export class SidebarComponent {
   public readonly listItems: IListItems[] = listItems;
+  public boards$ = this.store.select(selectBoards);
+  private boardId!: string;
 
-  constructor(public headerService: HeaderService) {}
+  constructor(public headerService: HeaderService, private store: Store, private router: Router) {
+    this.store.dispatch(getBoards());
+  }
+
+  public openBoard(selectedBoard: IBoard): void {
+    this.boards$.subscribe((boards) =>
+      boards.find((board) => {
+        if (board._id === selectedBoard._id) {
+          this.boardId = board._id as string;
+        }
+      }),
+    );
+    this.router.navigate([`boards/${this.boardId}`]);
+  }
 }
