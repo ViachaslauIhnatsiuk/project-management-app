f6ae43de1a709da31e62cccd5631523eaf2a47ff
Viachaslau <viachaslauihnatsiuk@gmail.com>
2022-11-15 18:15:08 +0300

    refactor: refactor adaptive for header and footer

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { githubAccounts } from 'src/app/core/constants/footer.constants';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { githubAccounts, githubIcon, rssLogo } from 'src/app/core/constants/footer.constants';
 import { IGithubAccount } from 'src/app/core/models/footer.models';
 
 @Component({
@@ -9,4 +11,9 @@ import { IGithubAccount } from 'src/app/core/models/footer.models';
 })
 export class FooterComponent {
   public readonly githubAccounts: IGithubAccount[] = githubAccounts;
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIconLiteral('githubIcon', sanitizer.bypassSecurityTrustHtml(githubIcon));
+    iconRegistry.addSvgIconLiteral('rssLogo', sanitizer.bypassSecurityTrustHtml(rssLogo));
+  }
 }
