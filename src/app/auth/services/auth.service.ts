70dc711f012bed98ef2db9dc1c5df9024d15ba79
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-11-15 23:09:12 +0300

    fix: remove dispatch from effects

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
 import jwtDecode from 'jwt-decode';
 import {
   ILogInRequest,
@@ -9,19 +11,33 @@ import {
   ISignUpResponse,
 } from 'src/app/core/models/auth-interceptor.models';
 import { IJWTPayload, MagicNumbers } from 'src/app/auth/models/auth-service.models';
+import { getUser, logIn } from '../store/actions/auth.actions';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private store: Store) {}
 
   public signUp(fields: ISignUpRequest): Observable<ISignUpResponse> {
-    return this.http.post<ISignUpResponse>('signup', fields);
+    return this.http.post<ISignUpResponse>('signup', fields).pipe(
+      map((response) => {
+        const { login, password } = fields;
+        this.store.dispatch(logIn({ login, password }));
+        return response;
+      }),
+    );
   }
 
   public signIn(fields: ILogInRequest): Observable<ILogInResponse> {
-    return this.http.post<ILogInResponse>('signin', fields);
+    return this.http.post<ILogInResponse>('signin', fields).pipe(
+      map((response) => {
+        localStorage.setItem('token', response.token);
+        const { id: userId } = this.getUserDataFromToken(response.token);
+        this.store.dispatch(getUser({ userId }));
+        return response;
+      }),
+    );
   }
 
   public getUser(id: string) {
