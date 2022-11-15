d55a9c2c15948789964a2e4c331bd435022dce61
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-11-15 23:09:51 +0300

    fix: remove dispatch getUser from BoardsPageComponent

--- src/app/board/boards/pages/boards-page/boards-page.component.ts
+++ src/app/board/boards/pages/boards-page/boards-page.component.ts
@@ -1,19 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { AuthService } from 'src/app/auth/services/auth.service';
-import { getUser } from 'src/app/auth/store/actions/auth.actions';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-boards-page',
   templateUrl: './boards-page.component.html',
   styleUrls: ['./boards-page.component.scss'],
 })
-export class BoardsPageComponent implements OnInit {
-  constructor(private authService: AuthService, private store: Store) {}
-
-  ngOnInit(): void {
-    const TOKEN = window.localStorage.getItem('token') as string;
-    const { id } = this.authService.getUserDataFromToken(TOKEN);
-    this.store.dispatch(getUser({ userId: id }));
-  }
-}
+export class BoardsPageComponent {}
