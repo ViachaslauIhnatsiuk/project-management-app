76785d64316724819a6fc08839867a5f7240afdd
GlebKolyano <glebkolyano38@gmail.com>
2022-11-18 14:07:08 +0300

    refactor: refactor modules

--- src/app/board/boards/pages/project-page/project-page.component.ts
+++ src/app/board/boards/pages/project-page/project-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+
 import { getPointsByUserId } from 'src/app/points/store/actions/points.actions';
 import { getUsers } from 'src/app/users/store/actions/users.actions';
 import { selectUser } from 'src/app/users/store/selectors/users.selectors';
@@ -11,14 +13,17 @@ import { getBoardById } from '../../store/actions/boards.actions';
   templateUrl: './project-page.component.html',
   styleUrls: ['./project-page.component.scss'],
 })
-export class ProjectPageComponent {
+export class ProjectPageComponent implements OnDestroy {
   private users$ = this.store.select(selectUser);
+  private userSubscription = new Subscription();
+
+  private boardIdSubscription = new Subscription();
 
   constructor(private store: Store, private route: ActivatedRoute) {
     this.getBoardById();
     this.store.dispatch(getUsers());
 
-    this.users$.subscribe((user) => {
+    this.userSubscription = this.users$.subscribe((user) => {
       if (user && user._id) {
         this.store.dispatch(getPointsByUserId({ userId: user._id }));
       }
@@ -26,8 +31,13 @@ export class ProjectPageComponent {
   }
 
   private getBoardById(): void {
-    this.route.params.subscribe((params) => {
+    this.boardIdSubscription = this.route.params.subscribe((params) => {
       this.store.dispatch(getBoardById({ boardId: params['id'] }));
     });
+  }
+
+  public ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+    this.boardIdSubscription.unsubscribe();
   }
 }
