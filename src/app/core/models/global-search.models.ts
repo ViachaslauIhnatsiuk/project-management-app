f1af24d23a31ca59822b84bd974847e98008d57a
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-11-20 14:21:56 +0300

    feat: add search service and interceptor

---
+++ src/app/core/models/global-search.models.ts
@@ -0,0 +1,10 @@
+export interface ISearchResponseItem {
+  _id: string;
+  title: string;
+  order: number;
+  boardId: string;
+  columnId: string;
+  description: string;
+  userId: string;
+  users: string[];
+}
