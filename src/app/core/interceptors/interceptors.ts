8dc4b14c2b0c259bac5f172a70a18153ac70590b
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-11-20 14:21:56 +0300

    feat: add search service and interceptor

--- src/app/core/interceptors/interceptors.ts
+++ src/app/core/interceptors/interceptors.ts
@@ -4,6 +4,7 @@ import { UsersInterceptor } from 'src/app/core/interceptors/users.interceptor';
 import { BoardsInterceptor } from 'src/app/core/interceptors/boards.interceptor';
 import { AppInterceptor } from './app.interceptor';
 import { PointsInterceptor } from './points.interceptor';
+import { SearchInterceptor } from './search.interceptor';
 
 export const httpInterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: AppInterceptor, multi: true },
@@ -11,4 +12,5 @@ export const httpInterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: UsersInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: BoardsInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: PointsInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: SearchInterceptor, multi: true },
 ];
