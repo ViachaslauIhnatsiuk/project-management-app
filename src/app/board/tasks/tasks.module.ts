921bb5b3eb62e88a1743f55374d2e65f52c389df
Viachaslau <viachaslauihnatsiuk@gmail.com>
2022-11-21 16:09:26 +0300

    feat: translate columns

--- src/app/board/tasks/tasks.module.ts
+++ src/app/board/tasks/tasks.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
-
+import { TranslateModule } from '@ngx-translate/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { EditTaskModalComponent } from './components/edit-task-modal/edit-task-modal.component';
 import { CreateTaskComponent } from './components/create-task/create-task.component';
@@ -28,6 +28,7 @@ import { PointsModule } from 'src/app/points/points.module';
     EffectsModule.forFeature([TaskEffects]),
     UsersModule,
     PointsModule,
+    TranslateModule,
   ],
   exports: [TasksComponent],
   providers: [TaskService],
