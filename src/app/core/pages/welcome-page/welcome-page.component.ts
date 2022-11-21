7feaae36a542af21107c07045f88e5476991ae40
Viachaslau <viachaslauihnatsiuk@gmail.com>
2022-11-21 13:02:23 +0300

    feat: translate login page

--- src/app/core/pages/welcome-page/welcome-page.component.ts
+++ src/app/core/pages/welcome-page/welcome-page.component.ts
@@ -1,26 +1,8 @@
 import { Component } from '@angular/core';
-import {
-  kanbanPracticeCards,
-  productivityList,
-  cusomizeParagraphs,
-  rssParagraphs,
-  developers,
-} from 'src/app/core/constants/welcome-page.constants';
-import { IKanbanPracticeCard, IDeveloper } from 'src/app/core/models/welcome-page.models';
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
   styleUrls: ['./welcome-page.component.scss'],
 })
-export class WelcomePageComponent {
-  public readonly kanbanPracticeCards: IKanbanPracticeCard[] = kanbanPracticeCards;
-
-  public readonly productivityList: string[] = productivityList;
-
-  public readonly cusomizeParagraphs: string[] = cusomizeParagraphs;
-
-  public readonly rssParagraphs: string[] = rssParagraphs;
-
-  public readonly developers: IDeveloper[] = developers;
-}
+export class WelcomePageComponent {}
