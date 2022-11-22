55f82d1cc61db40a98037c924a2f7854fb5a2280
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-11-22 20:25:29 +0300

    feat: add resp-message handler to points requests

---
+++ src/app/core/models/points-interceptor.model.ts
@@ -0,0 +1,7 @@
+export enum PointsResponseMessages {
+  Default = 'Unexpected response message',
+  Founded = 'Founded points',
+  Created = 'Created point',
+  Updated = 'Updated column',
+  Deleted = 'Deleted point',
+}
