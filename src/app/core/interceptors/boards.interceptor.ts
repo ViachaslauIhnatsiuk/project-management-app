359e5e3b3319311be6b8b248ecd5ef6e512d1a4e
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-11-22 20:26:51 +0300

    feat: add error handler to boards-interceptor

--- src/app/core/interceptors/boards.interceptor.ts
+++ src/app/core/interceptors/boards.interceptor.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 import { BASE_URL } from 'src/app/core/constants/interceptors.constants';
 import { ApiEndpoints, Methods } from '../models/interceptors.models';
+import { ResponseHandlerService } from '../services/response-handler.service';
 
 @Injectable()
 export class BoardsInterceptor implements HttpInterceptor {
+  constructor(private responseHandlerService: ResponseHandlerService) {}
+
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     if (
       request.url.includes(ApiEndpoints.Boards) ||
@@ -15,7 +24,18 @@ export class BoardsInterceptor implements HttpInterceptor {
       (request.url.includes(ApiEndpoints.TasksSet) && request.method === Methods.Patch)
     ) {
       const url = `${BASE_URL}${request.url}`;
-      return next.handle(request.clone({ url }));
+      return next.handle(request.clone({ url })).pipe(
+        catchError((error) => {
+          if (error instanceof HttpErrorResponse) {
+            const {
+              status,
+              error: { message },
+            } = error;
+            this.responseHandlerService.handleResponse(status, message);
+          }
+          return throwError(() => error);
+        }),
+      );
     }
 
     return next.handle(request);
