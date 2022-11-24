fdf01607a397c7fae0514b7331344f3cb0da0c34
Viachaslau <viachaslauihnatsiuk@gmail.com>
2022-11-24 13:12:49 +0300

    feat: add box-shadow to sidebar dots

--- src/app/shared/directives/generate-color.directive.ts
+++ src/app/shared/directives/generate-color.directive.ts
@@ -12,6 +12,9 @@ export class GenerateColorDirective {
     const generatedColor = `#${Math.floor(Math.random() * 2 ** 24)
       .toString(16)
       .padStart(6, '0')}`;
+    if (element.nativeElement.classList.contains('list__dot')) {
+      element.nativeElement.style.boxShadow = `0px 0px 4px 3px ${generatedColor}33`;
+    }
     element.nativeElement.style.backgroundColor = generatedColor;
   }
 }
