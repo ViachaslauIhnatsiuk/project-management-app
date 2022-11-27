d912f936e292e30ab092ad1a1d7ed38d2f2d13c9
Viachaslau <viachaslauihnatsiuk@gmail.com>
2022-11-27 14:54:35 +0300

    fix: fix bug with dark backgrounds with light theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { DOCUMENT } from '@angular/common';
+import { AfterViewChecked, Component, Inject, OnInit } from '@angular/core';
 import { HeaderService } from 'src/app/core/services/header.service';
 import { Themes } from './core/models/core.models';
 
@@ -7,15 +9,33 @@ import { Themes } from './core/models/core.models';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  constructor(private headerService: HeaderService) {}
-
-  @HostBinding('class') get themeMode() {
-    return this.headerService.isDarkTheme ? Themes.Dark : Themes.Light;
-  }
+export class AppComponent implements OnInit, AfterViewChecked {
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private headerService: HeaderService,
+    private overlayContainer: OverlayContainer,
+  ) {}
 
   ngOnInit() {
     this.headerService.getCurrentLanguage();
     this.headerService.getCurrentTheme();
+  }
+
+  ngAfterViewChecked() {
+    this.resetTheme();
+  }
+
+  resetTheme() {
+    if (this.headerService.theme === Themes.Light) {
+      this.document.body.classList.remove(Themes.Dark);
+      this.overlayContainer.getContainerElement().classList.remove(Themes.Dark);
+      this.document.body.classList.add(Themes.Light);
+      this.overlayContainer.getContainerElement().classList.add(Themes.Light);
+    } else {
+      this.document.body.classList.remove(Themes.Light);
+      this.overlayContainer.getContainerElement().classList.remove(Themes.Light);
+      this.document.body.classList.add(Themes.Dark);
+      this.overlayContainer.getContainerElement().classList.add(Themes.Dark);
+    }
   }
 }
